0e1811f148afba6647c02d6a57d87f0207b6f887
malikshubham827 <malikshubham827@gmail.com>
2017-11-02 19:01:02 +0530

    Added website files

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(function(){
+      $("#info").typed({
+        strings: ["Undergraduate student at Delhi University", "Competitive Programmer", "Machine Learning enthusiast"],
+        typeSpeed: 30,
+		backDelay: 500,
+		loop: true,
+		showCursor: true,
+		cursorChar: '_'
+      });
+  });
